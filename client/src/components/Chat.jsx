c5a4d70bbf29f1ec959e9bcebe76a8e32cb274c8
Benjamin Machock <bmachock@outlook.com>
2019-11-08 23:56:35 -0700

    added code for chat manager in chat.jsx

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,6 +1,37 @@
 import React, { Component } from "react";
 
 class Chat extends Component {
+  componentDidMount() {
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator: "",
+      userId: this.props.currentUsername,
+      tokenProvider: new Chatkit.TokenProvider({
+        url: "http://localhost:3001/authenticate"
+      })
+    });
+
+    chatManager
+      .connect()
+      .then(currentUser => {
+        this.setState({ currentUser });
+        return currentUser.subscribeToRoom({
+          roomId: "",
+          messageLimit: 6,
+          hooks: {
+            onMessage: message => {
+              this.setState({
+                messages: [...this.state.messages, message]
+              });
+            },
+            onUserJoined: () => this.forceUpdate()
+          }
+        });
+      })
+      .then(currentRoom => {
+        this.setState({ currentRoom });
+      })
+      .catch(error => console.error("error", error));
+  }
   render() {
     return (
       <div>
